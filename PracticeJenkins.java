6d1a8ce23a54277edacb6a56e044c9ef4eb78441
Stacey Whitfield <staceywhitfield@Staceys-MacBook.local>
2019-01-11 13:23:11 -0500

    first commit

---
+++ PracticeJenkins.java
@@ -0,0 +1,20 @@
+
+public class PracticeJenkins {
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.print("Having fun practicing Jenkins!");
+
+		}
+
+	}
+
+	//Once it is all pushed to GitHub 
+	//Add your GitHub Repo
+	//In the build Shell with the following commands
+	//javac <Filename>.java
+	//java <Filename>
+
+
